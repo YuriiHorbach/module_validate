338aea160974c2a7565278063a5d3c310916c27e
YuriiH <gyrii84@gmail.com>
2022-02-23 12:05:12 +0200

    check with rubocop

--- task3.rb
+++ task3.rb
@@ -1,47 +1,51 @@
-  module Validations 
-    def validate_presence *attr
-      @@valid_params = []
-      @@valid_params << attr
-    end
-    
+# frozen_string_literal: true
 
-    def self.included base
-      base.extend Validations
-    end
+# require 'rubocop'
 
-    def valid? 
-      if @params.empty? == false
-        @params.each do |el, key|
-          @@valid_params.each do |val|
-            if val.include? el
-              return false 
-            else
-              return true
-            end 
+module Validations
+  def validate_presence(*attr)
+    @@valid_params = []
+    @@valid_params << attr
+  end
+
+  def self.included(base)
+    base.extend Validations
+  end
+
+  def valid?
+    if @params.empty? == false
+      @params.each do |el, _key|
+        return false if el.value? ''
+
+        @@valid_params.each do |val|
+          if val.include? el
+            return false
+          else
+            return true
           end
         end
-      else
-        return false
       end
+    else
+      false
     end
   end
+end
 
-  class User
-    include Validations
+class User
+  include Validations
 
-    attr_accessor :name
-  
-    validate_presence :name
+  attr_accessor :name
 
-    def initialize *params 
-      @params = params
-    end
+  validate_presence :name
 
-   
+  def initialize(*params)
+    @params = params
   end
+end
 
-user = User.new 
+user = User.new name: 'aaa' # will be true
+user1 = User.new name: ''   # will be false
+user2 = User.new            # will be false
 p user.valid?
-
-
-
+p user1.valid?
+p user2.valid?
